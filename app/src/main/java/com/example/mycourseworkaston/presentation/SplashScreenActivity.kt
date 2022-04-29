660a503212fd1c795b97e79da5051b1fee80ccc3
unknown <89380041414@gmail.com>
2022-04-29 14:36:26 +0300

    Добавил splash screen, добавлена функция проверки подключения к интернету

---
+++ app/src/main/java/com/example/mycourseworkaston/presentation/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.example.mycourseworkaston.presentation
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mycourseworkaston.databinding.ActivitySplashScreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imSplashScreen.alpha = 0f
+        binding.imSplashScreen.animate().setDuration(1700).alpha(1f).withEndAction{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
