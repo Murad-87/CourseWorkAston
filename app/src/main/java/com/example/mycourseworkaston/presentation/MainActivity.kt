f24b19f5cede497f4832a760461e91ba1fb2d241
unknown <89380041414@gmail.com>
2022-05-04 11:49:38 +0300

    Реализовано bottomNavigationView

--- app/src/main/java/com/example/mycourseworkaston/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mycourseworkaston/presentation/MainActivity.kt
@@ -1,29 +1,48 @@
 package com.example.mycourseworkaston.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.mycourseworkaston.R
 import com.example.mycourseworkaston.databinding.ActivityMainBinding
 import com.example.mycourseworkaston.presentation.charactersFragment.CharactersFragment
+import com.example.mycourseworkaston.presentation.episodesFragment.EpisodesFragment
+import com.example.mycourseworkaston.presentation.locationsFragment.LocationsFragment
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.activity_frame_layout, CharactersFragment())
-            .commit()
+        val charactersFragment = CharactersFragment()
+        val episodesFragment = EpisodesFragment()
+        val locationsFragment = LocationsFragment()
+
+        openFragment(charactersFragment)
+
+//        supportFragmentManager
+//            .beginTransaction()
+//            .replace(R.id.activity_frame_layout, charactersFragment)
+//            .commit()
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.bottom_menu_characters->openFragment(charactersFragment)
+                R.id.bottom_menu_episodes -> openFragment(episodesFragment)
+                R.id.bottom_menu_locations -> openFragment(locationsFragment)
+            }
+            true
+        }
     }
 
-    fun openFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(
-            R.id.activity_frame_layout, fragment
-        )
-            .addToBackStack("Init")
-            .commit()
-    }
+    private fun openFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.activity_frame_layout, fragment)
+            addToBackStack("Init")
+            commit()
+        }
 }
--- app/src/main/java/com/example/mycourseworkaston/presentation/charactersFragment/CharactersFragment.kt
+++ app/src/main/java/com/example/mycourseworkaston/presentation/charactersFragment/CharactersFragment.kt
@@ -5,4 +5,7 @@ import com.example.mycourseworkaston.utils.BaseFragment
 
 class CharactersFragment :
     BaseFragment<FragmentCharactersBinding>(FragmentCharactersBinding::inflate) {
+
+
+
 }
